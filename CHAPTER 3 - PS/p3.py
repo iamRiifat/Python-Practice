059b0feea38b1ed3c2da5e8654940c0688278eab
Sakibul Hassan Rifat <92259707+iamRiifat@users.noreply.github.com>
2024-07-09 01:01:43 +0600

    push

---
+++ CHAPTER 3 - PS/p3.py
@@ -0,0 +1,3 @@
+#Write a program to detect double space in a string.
+naam="Amar naam rifat  "
+print(naam.find("  "))
---
+++ CHAPTER 3 - PS/p4.py
@@ -0,0 +1,3 @@
+#Replace the double space from problem 3 with single spaces.
+naam="Amar naam rifat  chilo"
+print(naam.replace("  "," "))
